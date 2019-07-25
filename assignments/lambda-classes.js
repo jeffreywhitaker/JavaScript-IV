2014f1bd01437957ebe8c3d196351023ffc2c4f1
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-07-25 13:54:13 -0700

    checking my work

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,4 @@
-// CODE here for your Lambda Classes
+// Classes
 
 class Person {
     constructor(attributes){
@@ -7,7 +7,7 @@ class Person {
         this.location = attributes.location;
     };
     greeting(){
-        return `Hello, my name is ${this.name} and I am from ${this.location}!}`
+        return `Hello, my name is ${this.name} and I am from ${this.location}!`
     };
 };
 
@@ -21,8 +21,8 @@ class Instructor extends Person {
     demo(subject){
         return `Today we are learning about ${subject}.`
     };
-    grade(student){
-        return `${student.name} receives a perfect score on ${subject}!`
+    grade(subject, student){
+        return `${student} receives a perfect score on ${subject}!`
     };
 };
 
@@ -34,13 +34,13 @@ class Student extends Person {
         this.favSubjects = attributes.favSubjects;
     };
     listsSubjects(){
-        return `${student.name}'s favorite subjects are: ${this.favSubjects}!`
+        return `${this.name}'s favorite subjects are: ${this.favSubjects}!`
     };
     PRAssignment(subject){
-        return `${student.name} has submitted a PR for ${subject}.`
+        return `${this.name} has submitted a PR for ${subject}.`
     };
     sprintChallenge(subject){
-        return `${student.name} has begun sprint challenge on ${subject}.`
+        return `${this.name} has begun sprint challenge on ${subject}.`
     };
 };
 
@@ -57,3 +57,59 @@ class ProjectManager extends Instructor {
         return `${this.name} debugs ${student.name}'s code on ${subject}.`
     };
 };
+
+// Objects
+
+const instructor1 = new Instructor({
+    name: 'Brit Hemming',
+    age: 'she will never tell',
+    location: 'Ottawa',
+    specialty: 'breakout sessions',
+    favLanguage: 'Javascript',
+    catchPhrase: 'it is time to do a breakout session!',
+});
+
+const instructor2 = new Instructor({
+    name: 'Dan Frehner',
+    age: '50ish',
+    location: 'unknown',
+    specialty: 'making funny videos',
+    favLanguage: 'Python',
+    catchPhrase: 'Thank you for watching this video',
+});
+
+const student1 = new Student({
+    name: 'Jeffrey Whitaker',
+    age: 30,
+    location: 'Oregon',
+    previousBackground: 'History student',
+    className: 'Web22',
+    favSubjects: ['history', 'coding', 'science'],
+});
+
+const student2 = new Student({
+    name: 'Cameron Alvarado',
+    age: 'mid 20s',
+    location: 'Washington',
+    previousBackground: 'unknown',
+    className: 'Web22',
+    favSubjects: ['psychology', 'cosmetics', 'underwater basked weaving'],
+});
+
+const pm1 = new ProjectManager({
+    name: 'Jonathan Taylor',
+    age: 28,
+    location: 'Alabama',
+    specialty: 'being chill',
+    favLanguage: 'English',
+    catchPhrase: 'cool',
+    gradClassName: 'WEB20',
+    favInstructor: 'Dan Frehner',
+});
+
+// Logs
+
+console.log(instructor1.age);
+console.log(instructor1.grade('JS IV', 'Jeff'));
+console.log(student2.greeting());
+console.log(student1.listsSubjects());
